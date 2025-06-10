83ab9a7c6b0db23095d54ba65260a74a26fe561e
rakesh <rakeshsagartfi21@gmail.com>
2025-06-10 12:01:56 +0000

    Added CSS Styles

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -7,17 +7,29 @@ import './index.css'
 class Speedometer extends Component {
   render() {
     return (
-      <div>
-        <h1> SPEEDOMETER </h1>
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
-          alt="speedometer"
-        />
-        <h1> Speed is 0mph </h1>
-        <p> Min Limit is 0mph, Max Limit is 200mph </p>
-        <div>
-          <button> Accelerate </button>
-          <button> Apply Brake </button>
+      <div className="speedometer-app-container">
+        <div className="speedometer-container">
+          <h1 className="heading"> SPEEDOMETER </h1>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+            alt="speedometer"
+            className="speedometer-image"
+          />
+          <h1 className="speed-text"> Speed is 0mph </h1>
+          <p className="speed-limits">
+            {' '}
+            Min Limit is 0mph, Max Limit is 200mph{' '}
+          </p>
+          <div className="buttons-container">
+            <button type="button" className="accelerate-button button">
+              {' '}
+              Accelerate{' '}
+            </button>
+            <button type="button" className="apply-break-button button">
+              {' '}
+              Apply Brake{' '}
+            </button>
+          </div>
         </div>
       </div>
     )
