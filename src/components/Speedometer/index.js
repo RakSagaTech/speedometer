8084a87d160da84832ae4100626adae3b31d7e59
rakesh <rakeshsagartfi21@gmail.com>
2025-06-10 12:15:22 +0000

    Added Functions for Buttons

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -5,7 +5,28 @@ import {Component} from 'react'
 import './index.css'
 
 class Speedometer extends Component {
+  state = {
+    speed: 0,
+  }
+
+  onClickApplyBrakeButton = () => {
+    const {speed} = this.state
+
+    if (speed > 0) {
+      this.setState(prevState => ({speed: prevState.speed - 10}))
+    }
+  }
+
+  onClickAccelerateButton = () => {
+    const {speed} = this.state
+
+    if (speed < 200) {
+      this.setState(prevState => ({speed: prevState.speed + 10}))
+    }
+  }
+
   render() {
+    const {speed} = this.state
     return (
       <div className="speedometer-app-container">
         <div className="speedometer-container">
@@ -15,17 +36,25 @@ class Speedometer extends Component {
             alt="speedometer"
             className="speedometer-image"
           />
-          <h1 className="speed-text"> Speed is 0mph </h1>
+          <h1 className="speed-text"> Speed is {speed}mph </h1>
           <p className="speed-limits">
             {' '}
             Min Limit is 0mph, Max Limit is 200mph{' '}
           </p>
           <div className="buttons-container">
-            <button type="button" className="accelerate-button button">
+            <button
+              type="button"
+              className="accelerate-button button"
+              onClick={this.onClickAccelerateButton}
+            >
               {' '}
               Accelerate{' '}
             </button>
-            <button type="button" className="apply-break-button button">
+            <button
+              type="button"
+              className="apply-break-button button"
+              onClick={this.onClickApplyBrakeButton}
+            >
               {' '}
               Apply Brake{' '}
             </button>
